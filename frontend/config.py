003c58a233b708a66e998d9dac1c4f002bf6eb7b
mms <1605585155@qq.com>
2026-03-29 22:25:30 +0800

    ai聲明

--- frontend/config.py
+++ frontend/config.py
@@ -1,7 +1,7 @@
 """全局配置常量"""
 
 # 后端 API 地址
-API_BASE_URL = "http://127.0.0.1:8000"
+API_BASE_URL = "https://121.43.27.209/8000"
 #https://jaded-unpublic-mignon.ngrok-free.dev/
 # 页面路由名称
 PAGE_HOME = "🏠 首页"
